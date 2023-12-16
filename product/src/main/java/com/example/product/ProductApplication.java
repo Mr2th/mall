79767b00c0c2f000c9cb82267a8ad545ca07835e
Mr2th <httxjg123@gmail.com>
2023-12-16 15:56:46 +0800

    init

---
+++ product/src/main/java/com/example/product/ProductApplication.java
@@ -0,0 +1,15 @@
+package com.example.product;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = "com.example.product.dao")
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
